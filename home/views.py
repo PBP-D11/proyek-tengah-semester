9780c160456902d35f2a3b918afdfa088646fe50
nadhifralfan <nadhif.rahman11@ui.ac.id>
2022-10-29 01:31:20 +0700

    Update user auth V1

--- home/views.py
+++ home/views.py
@@ -1,5 +1,41 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
+from django.contrib import messages
+from .forms import CustomUserCreationForm
+from django.contrib.auth import logout
+from django.http import HttpResponseRedirect
+from django.core import serializers
 
 # Create your views here.
 def main(request):
     return render(request, 'index.html')
+
+
+def register(request):
+    form = CustomUserCreationForm()
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Akun telah berhasil dibuat!')
+            return HttpResponseRedirect('/login/')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home:main')
+        else:
+            messages.info(request, 'Username atau Password salah!')
+    context = {}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    return redirect('home:main')
--- project_tengah_semester/settings.py
+++ project_tengah_semester/settings.py
@@ -145,3 +145,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
+AUTH_USER_MODEL = 'home.CustomUser'
