554b7b4bacb6363c33ca12aaf805463c3f6d4f90
Michael Baptiswa Marully <baptiswamichael@gmail.com>
2022-11-02 22:36:31 +0700

    Forum blm fix

--- project_tengah_semester/urls.py
+++ project_tengah_semester/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('find-charge/', include('findcharge.urls'))
+    path('find-charge/', include('findcharge.urls')),
+    path('evorums/', include('evorumss.urls'))
 ]
+
+# path('services/', include('evices.urls')),
